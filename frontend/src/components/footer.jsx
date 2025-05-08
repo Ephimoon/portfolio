2db90d21a833ebdf14cf45830e9660762df7adaf
Melanie Escobar <157929578+Ephimoon@users.noreply.github.com>
2025-05-08 17:15:18 -0500

    added footer

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+    const footerBarItems = [
+        { title: 'resume', url: "/cert.pdf" },
+        { title: 'github', url: "https://github.com/Ephimoon" },
+        { title: 'linkedIn', url: "https://www.linkedin.com/in/melanie-escobar-marulanda" }
+      ];
+
+    return (
+        <nav className="bg-first w-full">
+        <div className="max-w-screen-xl mx-auto p-4">
+            <div className="md:flex md:items-center md:justify-between md:py-1">
+
+                <h2 className='p-2 text-second font-gaegu'>thank you for stopping by /ᐠ｡ꞈ｡ᐟ✿\</h2>
+
+                <div className={"flex flex-row items-center gap-3 font-gaegu text-second text-sm p-4 md:p-1"}>
+                    {footerBarItems.map((x, index) => {
+                        return (
+                            <a
+                                key={index}
+                                href={x.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="relative inline-block group p-1"
+                                >
+                                {x.title}
+                            </a>
+                        );
+                    })}
+                </div>
+
+                <h3 className='p-2 font-mono text-second text-sm'>© made with 💚 by melanie :)</h3>
+            </div>
+
+        </div>
+        </nav>
+    )
+}
+
+export default Footer
--- frontend/src/sections/contact.jsx
+++ frontend/src/sections/contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from '../components/footer.jsx'
 
 const Contact = () => {
-    return (
-        <section id="contact" className="min-h-screen snap-start flex items-center justify-center bg-indigo-700">
-            Contact Section
-        </section>
-    )
+  return (
+    <section id="contact" className="min-h-screen snap-start flex flex-col justify-center bg-indigo-700">
+      <div className="flex-grow flex items-center justify-center text-white">
+        Contact Section
+      </div>
+      <Footer />
+    </section>
+  )
 }
 
 export default Contact
